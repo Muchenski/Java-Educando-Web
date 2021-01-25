bc00e4b97c58b3f419055f3b8a24da8e5449682f
Muchenski <muchenski.dev@gmail.com>
2021-01-25 18:10:14 -0300

    Imprimindo as peças com cores diferentes na tela

--- Seção 16 - Projeto Xadrez/ProjetoXadrez/src/application/UI.java
+++ Seção 16 - Projeto Xadrez/ProjetoXadrez/src/application/UI.java
@@ -2,10 +2,31 @@ package application;
 
 import boardgame.Board;
 import boardgame.Piece;
+import chess.ChessPiece;
+import chess.enums.Color;
 
 // Classe responsável para realizar as modificações
 // de exibição da interface na tela.
 public class UI {
+
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
 	public static void printBoard(Piece[][] pieces, Board board) {
 
@@ -29,9 +50,16 @@ public class UI {
 
 	public static void printPiece(Piece piece) {
 		if (piece == null) {
-			System.out.print("- ");
+			System.out.print("-");
 		} else {
-			System.out.print(piece + " ");
+			if (piece instanceof ChessPiece) {
+				if (((ChessPiece) piece).getColor() == Color.WHITE) {
+					System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+				} else {
+					System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+				}
+			}
 		}
+		System.out.print(" ");
 	}
 }
--- Seção 16 - Projeto Xadrez/ProjetoXadrez/src/chess/ChessMatch.java
+++ Seção 16 - Projeto Xadrez/ProjetoXadrez/src/chess/ChessMatch.java
@@ -31,9 +31,20 @@ public class ChessMatch {
 	}
 
 	private void initialSetup() {
-		placePiece(new ChessPosition('b', 6), new Rook(board, Color.WHITE));
-		placePiece(new ChessPosition('e', 8), new King(board, Color.BLACK));
-		placePiece(new ChessPosition('e', 1), new King(board, Color.WHITE));
+
+		placePiece(new ChessPosition('c', 1), new Rook(board, Color.WHITE));
+		placePiece(new ChessPosition('c', 2), new Rook(board, Color.WHITE));
+		placePiece(new ChessPosition('d', 2), new Rook(board, Color.WHITE));
+		placePiece(new ChessPosition('e', 2), new Rook(board, Color.WHITE));
+		placePiece(new ChessPosition('e', 1), new Rook(board, Color.WHITE));
+		placePiece(new ChessPosition('d', 1), new King(board, Color.WHITE));
+
+		placePiece(new ChessPosition('c', 7), new Rook(board, Color.BLACK));
+		placePiece(new ChessPosition('c', 8), new Rook(board, Color.BLACK));
+		placePiece(new ChessPosition('d', 7), new Rook(board, Color.BLACK));
+		placePiece(new ChessPosition('e', 7), new Rook(board, Color.BLACK));
+		placePiece(new ChessPosition('e', 8), new Rook(board, Color.BLACK));
+		placePiece(new ChessPosition('d', 8), new King(board, Color.BLACK));
 	}
 
 	public Board getBoard() {
