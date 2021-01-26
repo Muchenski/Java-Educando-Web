94122fb8ccca86475997d5d5205269d1dc988566
Muchenski <muchenski.dev@gmail.com>
2021-01-25 21:25:35 -0300

    Implementando movimentos possíveis do rei

--- Seção 16 - Projeto Xadrez/ProjetoXadrez/src/chess/pieces/King.java
+++ Seção 16 - Projeto Xadrez/ProjetoXadrez/src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.enums.Color;
 
@@ -17,7 +18,63 @@ public class King extends ChessPiece {
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position auxPosition = new Position(0, 0);
+
+		// acima
+		auxPosition.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+
+		// abaixo
+		auxPosition.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+
+		// esquerda
+		auxPosition.setValues(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+
+		// direita
+		auxPosition.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+
+		// noroeste
+		auxPosition.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+
+		// nordeste
+		auxPosition.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+
+		// sudoeste
+		auxPosition.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+
+		// sudeste
+		auxPosition.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+
 		return mat;
+	}
+
+	private boolean canMove(Position position) {
+		ChessPiece piece = (ChessPiece) getBoard().getPiece(position);
+		return piece == null || piece.getColor() != this.getColor();
 	}
 
 	@Override
